99e0cbe2ad0aacaf8feca2899159d97e874af554
FirozTheProgrammer <firozhasan1542@gmail.com>
2024-06-05 11:12:53 +0600

    Call Back Function

---
+++ CallBack.js
@@ -0,0 +1,41 @@
+// call back function syntax
+
+// this function can displyed value of another function
+const disply = (result) => {
+  console.log(result);
+};
+
+// calculate values function
+const calculator = (num1, num2, callback) => {
+  let sum = num1 + num2;
+
+  // if have call back function then it will be run otherwise not
+  if (callback) callback(sum);
+};
+
+calculator(5, 5, disply);
+
+// different way to call back function
+
+const calculator2 = (num1, num2, callback) => {
+  let sum = num1 + num2;
+
+  // if have call back function then it will be run otherwise not
+  if (callback) callback(sum);
+};
+
+calculator2(5, 5, (result) => {
+  console.log(result);
+});
+
+// another call back function
+function fetchData(callback) {
+  setTimeout(() => {
+    const data = "Some data";
+    callback(data);
+  }, 1000);
+}
+
+fetchData((data) => {
+  console.log(data); // Outputs "Some data" after 1 second
+});
---
+++ README.md
@@ -0,0 +1,1 @@
+# Understanding-Asynchronous-JavaScript-function
